90def83511f2384a7012d7d3be6172ce01eff013
amar <yolob2112@gmail.com>
2017-06-17 17:15:13 +0530

    Initial commit

---
+++ jjj.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include "vector"
+using namespace std;
+
+int main()
+{
+    vector< int > x;
+    x.push_back(6);
+    x.push_back(8);
+
+    // push_back
+    int u=55;
+    u=77;
+    cin>>u;
+    // cout<<"damn";
+    // cout<<"damn";
+    // cout<<"damn";
+    int arr[10];
+    for(int i=0;i<5;i++)
+    arr[i]=77;
+    //clrscr();
+    return 0;
+    
+}
